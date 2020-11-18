bbbca15217588692c78b94c21b8cd5384387aebd
ludwigjer <36234519+ludwigjer@users.noreply.github.com>
2020-11-18 10:35:06 +0000

    Update TheThing.java

--- src/TheThing.java
+++ src/TheThing.java
@@ -1,5 +1,36 @@
 public class TheThing {
     public static void main(String[] args) {
         System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        System.out.println("I love pancakes!");
+        
     }
 }
